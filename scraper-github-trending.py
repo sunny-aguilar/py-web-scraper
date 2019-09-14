99db3b12a6d4256387c2d98f30d0d6fd8255e17e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-14 15:43:32 -0700

    ws

--- scraper-github-trending.py
+++ scraper-github-trending.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 # Get the page
 page = requests.get('https://github.com/trending')
-#print(page)
+print(page)
 
 # Create a BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -15,24 +15,36 @@ soup = BeautifulSoup(page.text, 'html.parser')
 repo = soup.find(class_="explore-pjax-container container-lg p-responsive pt-6")
 
 # Extracing data - find all instances of this class
-repo_list = repo.find_all(class_='Box-row')
+repo_list = repo.find_all(class_='h3 lh-condensed')
 
 print(len(repo_list))
+#print(repo_list[0])
+# print(repo_list[1])
+# print(repo_list[2])
+
+full_repo_name = repo_list[0].find('a').text.split('/')
+developer = full_repo_name[0].strip()
+developer = full_repo_name[1].strip()
+print(developer)
+# print('developer: ', developer)
+# print('name: ', repo_name)
 
 
-# get number of stars
-count = 0
-for repo in repo_list:
-  if count > 2:
-    dev_name = repo.find(class_='h3 lh-condensed').text
+# get developer and repo name
+#for repo in repo_list:
+    #dev_name = repo_list.find('a').text.split('/')
     #print(dev_name)
+    #print(count)
     #full_repo_name = repo.find('a').text.split('/')
     #print(full_repo_name)
     #developer = full_repo_name[0].strip()
 
-  count += 1
+
+
   #repo_name = full_repo_name[1].strip()
   #stars = repo.find(class_='octicon octicon-star').parent.text.strip()
   #print('developer', developer)
   #print('name', repo_name)
   #print('stars', stars)
+
+
