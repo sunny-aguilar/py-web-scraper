94e00727236a807d54d66e167075cddab2c9f177
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-14 14:18:57 -0700

    ws

--- scraper-github-trending.py
+++ scraper-github-trending.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 # Get the page
 page = requests.get('https://github.com/trending')
-print(page)
+#print(page)
 
 # Create a BeautifulSoup object
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -17,4 +17,4 @@ repo = soup.find(class_="explore-pjax-container container-lg p-responsive pt-6")
 # Extracing data - find all instances of this class
 repo_list = repo.find_all(class_='Box-row')
 
-print(repo_list)
+print(len(repo_list))
