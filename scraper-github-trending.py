bce130bd542b3fe0d470abc6309898b91a520ef5
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-14 15:53:00 -0700

    ws

--- scraper-github-trending.py
+++ scraper-github-trending.py
@@ -39,8 +39,8 @@ for repo in repo_list:
   repo_name = full_repo_name[1].strip()
 
   # get the starts for each repo
-  stars = repo.find(class_='octicon octicon-star').parent.text.strip()
-  
+  stars = repo.find(class_=' muted-link d-inline-block mr-3').text.strip()
+
   # display information
   print('developer: ', developer)
   print('name: ', repo_name)
