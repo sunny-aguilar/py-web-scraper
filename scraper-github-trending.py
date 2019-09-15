2395939a71137ba1db345b4074f551d3f6be7ecc
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-14 20:41:14 -0700

    ws

--- scraper-github-trending.py
+++ scraper-github-trending.py
@@ -1,4 +1,7 @@
-# webscraper
+# Webscraper using Python
+# Author: Sandro Aguilar
+# Created: Sept 14, 2019
+# Libraries Used: requests, BeautifulSoup, csv
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -36,13 +39,13 @@ for repo in repo_list:
   repo_name = full_repo_name[1].strip()
 
   # get the starts for each repo
-  stars = repo.find(class_='f6 text-gray mt-2').text
-  print(stars)
+  stars = repo.find(class_='octicon octicon-star').text
 
   # display information
   print('developer: ', developer)
   print('name: ', repo_name)
-  print('stars: ', stars)
+  print('stars: ', stars, '\n')
+
 
   # add the info as a row into the CSV file
   f.writerow([developer, repo_name])
